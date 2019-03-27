a0c3e1022bb5c51640d69028238362d61dd562b9
tshemsedinov <timur.shemsedinov@gmail.com>
2019-03-27 13:24:20 +0200

    Pass filename and options from Thread to Worker
    Refs: https://github.com/metarhia/metasync/issues/416

--- test/locks.js
+++ test/locks.js
@@ -1,28 +1,33 @@
 'use strict';
 
-const { isMainThread } = require('worker_threads');
-const { locks } = require('..');
-const { Thread } = locks;
-const metatests = require('metatests');
+const common = require('@metarhia/common');
+const nodeVerion = common.between(process.version, 'v', '.');
 
-const sleep = msec =>
-  new Promise(resolve => {
-    setTimeout(resolve, msec);
-  });
+if (nodeVerion >= 11) {
+  const { isMainThread } = require('worker_threads');
+  const { locks } = require('..');
+  const { Thread } = locks;
+  const metatests = require('metatests');
 
-if (isMainThread) {
-  metatests.test('locks: enter and leave', test => {
-    new Thread();
-    new Thread();
+  const sleep = msec =>
+    new Promise(resolve => {
+      setTimeout(resolve, msec);
+    });
 
-    setTimeout(() => {
-      locks.request('A', async lock => {
-        test.end();
-      });
-    }, 100);
-  });
-} else {
-  locks.request('A', async lock => {
-    await sleep(100);
-  });
+  if (isMainThread) {
+    metatests.test('locks: enter and leave', test => {
+      new Thread(__filename);
+      new Thread(__filename);
+
+      setTimeout(() => {
+        locks.request('A', async lock => {
+          test.end();
+        });
+      }, 100);
+    });
+  } else {
+    locks.request('A', async lock => {
+      await sleep(100);
+    });
+  }
 }
